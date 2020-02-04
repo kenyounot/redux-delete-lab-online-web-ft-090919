482ad6cea8d07989a4da8e04ffc0b62015b73b04
Kenneth Spatta <kenyounot@gmail.com>
2020-02-03 21:41:52 -0500

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
+	render() {
+		console.log(this.props.band.id);
 
-  render() {
-    return(
-      <div>
-        Band Component
-      </div>
-    );
-  }
-};
+		return (
+			<div>
+				Bands:
+				<li>
+					Name: {this.props.band.name}
+					<button onClick={() => this.props.delete(this.props.band)}>DELETE</button>
+				</li>
+			</div>
+		);
+	}
+}
 
 export default Band;
--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -1,23 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import BandInput from './BandInput';
-
-import { connect } from 'react-redux'
+import Band from './Band';
+import { connect } from 'react-redux';
 
 class BandsContainer extends Component {
-  render() {
-    return (
-      <div>
-        <BandInput addBand={this.props.addBand}/>
-
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<BandInput addBand={this.props.addBand} />
+				{this.props.bands.map(band => {
+					return <Band delete={this.props.deleteBand} key={band.id} band={band} />;
+				})}
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = ({ bands }) => ({ bands })
+const mapStateToProps = ({ bands }) => ({ bands });
 
 const mapDispatchToProps = dispatch => ({
-  addBand: name => dispatch({ type: "ADD_BAND", name })
-})
+	addBand: name => dispatch({ type: 'ADD_BAND', name }),
+	deleteBand: band => dispatch({ type: 'DELETE_BAND', id: band.id })
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer);
